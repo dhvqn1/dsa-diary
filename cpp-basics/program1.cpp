6df4aa7fe3891e2d5ad553e0ebc2ecb426793cb7
dhvqn1 <dhvaniiyer@outlook.com>
2026-05-26 14:46:16 +0000

    started cpp basics

---
+++ cpp-basics/program1.cpp
@@ -0,0 +1,12 @@
+//boilerplate
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world\n";
+    return 0;
+}
+
+//terminal commands to run the program
+//compiling - $ g++ filename.cpp -o filename
+// run - ./filename
