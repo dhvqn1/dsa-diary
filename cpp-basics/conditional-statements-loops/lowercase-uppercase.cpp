c39204fdb855cb903837cb79068ed1db6234d269
dhvqn1 <dhvaniiyer@outlook.com>
2026-05-28 14:23:15 +0000

    adding practice problem codes.

---
+++ cpp-basics/conditional-statements-loops/lowercase-uppercase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char ch;
+    cin >> ch;
+
+    if(ch >= 'A' && ch <= 'Z') {
+        cout << "uppercase";
+    }
+
+    else if(ch >= 'a' && ch <= 'z') {
+        cout << "lowercase";
+    }
+
+    else {
+        cout << "not an alphabet";
+    }
+
+    return 0;
+}
---
+++ cpp-basics/conditional-statements-loops/prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    bool isPrime = true;
+
+    for(int i = 2; i * i <= n; i++) {
+
+        if(n % i == 0) {
+            isPrime = false;
+            break;
+        }
+
+    }
+
+    if(isPrime) {
+        cout << "prime";
+    }
+
+    else {
+        cout << "non-prime";
+    }
+
+    return 0;
